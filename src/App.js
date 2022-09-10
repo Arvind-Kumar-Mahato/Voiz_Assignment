975e205f29ae5ac1d58e8947d8edc34f3966915c
Arvind-Kumar-Mahato <93374171+Arvind-Kumar-Mahato@users.noreply.github.com>
2022-09-10 21:53:38 +0530

    frontend task

--- src/App.js
+++ src/App.js
@@ -1,25 +1,31 @@
-import logo from './logo.svg';
-import './App.css';
+import Select from "react-select";
+import "./App.css";
+import React, { Component } from "react";
+import options from "./option.js";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+const styles = {
+  multiValue: (styles) => {
+    return {
+      ...styles,
+      backgroundColor: "papayawhip",
+    };
+  },
+};
+
+
+class App extends Component {
+  render() {
+    return (
+      <Select
+        styles={styles}
+        closeMenuOnSelect={false}
+        isMulti
+        options={options}
+        defaultValue={options[0]}
+      />
+    );
+  }
 }
 
 export default App;
